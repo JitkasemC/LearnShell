060a2b9e92c67761faedaad70ce955d00579f174
JitkasemC <jitkasem.c@gmail.com>
2021-12-26 04:47:31 +0700

    add BasicShell.sh

---
+++ BasicShell.sh
@@ -0,0 +1,33 @@
+#learning basic shell
+#touch = create file must follow with file name plus file extention.
+#touch test4.js
+#ls
+
+# PRICE_PER_APPLE=5
+# echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"
+
+#FILE_LIST=`ls`
+# echo this is File list ${FILE_LIST}
+
+# current=`pwd`
+# echo current directory is ${current}
+
+
+# echo this is File list `ls`
+
+# echo current directory is $(pwd)
+
+# FILELIST=`ls`
+# FileWithTimeStamp=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
+
+# echo ${FILELIST}
+# echo this is File with time stamp ${FileWithTimeStamp}
+
+# BIRTHDATE="Jan 1, 2000"
+# Presents=10
+# BIRTHDAY=`date -d "$BIRTHDATE" +%A`
+# echo Birthday is ${BIRTHDAY}
+
+git remote add origin https://github.com/JitkasemC/LearnJS.git
+git branch -M main
+git push -u origin main
